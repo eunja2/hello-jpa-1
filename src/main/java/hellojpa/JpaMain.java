0f14131c0ca9c52d90fda6991dcac01eec42f9a4
ej0514 <ej051444@gmail.com>
2023-10-31 17:04:18 +0900

    3. 영속성 관리 (1), (2)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -33,13 +33,44 @@ public class JpaMain {
 //            Member findMember = em.find(Member.class, 1L);
 //            em.remove(findMember);
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(0)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            for(Member member : result)
+//                System.out.println("member = "+ member.getName());
 
-            for(Member member : result)
-                System.out.println("member = "+ member.getName());
+            // 비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("helloJPA");
+
+            // 영속
+//            System.out.println("=== Before ===");
+//            em.persist(member);
+//            System.out.println("=== After ===");
+//
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+//
+//            System.out.println("result = " + (findMember1 == findMember2));
+
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("2222");
+
+//            em.persist(member);
+
+            System.out.println("==============");
 
             tx.commit();
         } catch(Exception e){
